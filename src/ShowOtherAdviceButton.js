44dbe4eaf2ba8216da23ce80ead61206bc06b364
NadiaPoi <nadia.poi.fou@gmail.com>
2019-05-27 15:09:17 +0200

    add app.js and tests

---
+++ src/ShowOtherAdviceButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export default function ShowOtherAdviceButton(){
+    return (
+        <button
+          className="Advice-other-advice-button"
+          onClick={this.loadAdvice}
+        >
+          Show other advice
+        </button>
+    )
+}
